928fd79ef5e53d1012d404a66c7732b9b6e67af9
Prasanna R <hello@prasannarehpade.com>
2023-08-23 00:26:59 +0530

    updated logic

--- Que_Fourth_Loop_rt_date.sql
+++ Que_Fourth_Loop_rt_date.sql
@@ -1,36 +1,37 @@
 DECLARE
-    dob DATE := TO_DATE('1994-07-27', 'YYYY-MM-DD');  -- Date of Birth
-    doj DATE := TO_DATE('2023-03-15', 'YYYY-MM-DD');  -- Date of Joining
-    retirement_age NUMBER := 65;  -- Retirement Age
-    months_remaining NUMBER;
-    retirement_date DATE;
-    current_month NUMBER;
-    current_year NUMBER;
+    v_dob DATE := TO_DATE('1994-07-27', 'YYYY-MM-DD');  -- Date of Birth
+    v_doj DATE := TO_DATE('2023-03-15', 'YYYY-MM-DD');  -- Date of Joining
+    v_retirement_age NUMBER := 65;  -- Retirement Age
+    v_months_remaining NUMBER;
+    v_retirement_date DATE;
+    v_current_month NUMBER;
+    v_current_year NUMBER;
 BEGIN
-    -- Calculate the total number of months between the birthdate and joining date
-    SELECT MONTHS_BETWEEN(doj, dob) INTO months_remaining FROM DUAL;
+    
+    SELECT MONTHS_BETWEEN(v_doj, v_dob) 
+    INTO   v_months_remaining 
+    FROM DUAL;
 
-    -- Calculate the current month and year
-    current_month := TO_NUMBER(TO_CHAR(doj, 'MM'));
-    current_year := TO_NUMBER(TO_CHAR(doj, 'YYYY'));
+    -- Extracting Month and Year
+    v_current_month := TO_NUMBER(TO_CHAR(v_doj, 'MM'));
+    v_current_year := TO_NUMBER(TO_CHAR(v_doj, 'YYYY'));
 
-    -- Iterate through the months until retirement age is reached
-    WHILE months_remaining > 0 LOOP
-        -- Calculate the retirement month and year for each iteration
-        IF current_month = 12 THEN
-            current_month := 1;  -- Cycle to January and increment the year
-            current_year := current_year + 1;
+    -- loop till remaining Months
+    WHILE v_months_remaining > 0 LOOP
+        -- for Month 1 to 12
+        IF v_current_month = 12 THEN
+            v_current_month := 1;  -- increment the year
+            v_current_year := v_current_year + 1;
         ELSE
-            current_month := current_month + 1;  -- Increment the month
+            v_current_month := v_current_month + 1;  -- Increment the month
         END IF;
 
-        months_remaining := months_remaining - 1;
+        v_months_remaining := v_months_remaining - 1;
     END LOOP;
 
-    -- Calculate the retirement date
-    retirement_date := TO_DATE(current_year || '-' || LPAD(current_month, 2, '0') || '-' || TO_CHAR(doj, 'DD'), 'YYYY-MM-DD');
+    -- Concat all to show in date format
+    v_retirement_date := TO_DATE(v_current_year || '-' || LPAD(v_current_month, 2, '0') || '-' || TO_CHAR(v_doj, 'DD'), 'YYYY-MM-DD');
 
-    -- Print the retirement date
-    DBMS_OUTPUT.PUT_LINE('Retirement Date: ' || TO_CHAR(retirement_date, 'DD-MM-YYYY'));
+    -- retirement date output
+    DBMS_OUTPUT.PUT_LINE('Retirement Date: ' || TO_CHAR(v_retirement_date, 'DD-MM-YYYY'));
 END;
-/
